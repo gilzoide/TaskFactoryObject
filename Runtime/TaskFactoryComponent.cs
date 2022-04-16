becefe814915c33dd375bbc330637112bd946be0
gilzoide <gilzoide@gmail.com>
2022-04-16 11:16:18 -0300

    Add MonoBehaviour create/destroy lifecycle config

--- Runtime/TaskFactoryComponent.cs
+++ Runtime/TaskFactoryComponent.cs
@@ -7,23 +7,86 @@ namespace Gilzoide.TaskFactoryObject
 {
     public class TaskFactoryComponent : MonoBehaviour, ITaskFactoryObject
     {
+        public enum CreateLifecycle
+        {
+            Awake,
+            Start,
+            OnEnable,
+        }
+
+        public enum DestroyLifecycle
+        {
+            OnDestroy,
+            OnDisable,
+        }
+
         public TaskFactoryConfig TaskFactoryConfig;
+        public CreateLifecycle CreateFactoryLifecycle;
+        public DestroyLifecycle DestroyFactoryLifecycle;
 
         public TaskFactoryConfig FactoryConfig => TaskFactoryConfig;
         public TaskFactory Factory { get; private set; }
 
         private CancellationTokenSource _cancellationTokenSource;
         
+        void Awake()
+        {
+            if (CreateFactoryLifecycle == CreateLifecycle.Awake)
+            {
+                CreateFactoryIfNeeded();
+            }
+        }
+
+        void Start()
+        {
+            if (CreateFactoryLifecycle == CreateLifecycle.Start)
+            {
+                CreateFactoryIfNeeded();
+            }
+        }
+
         void OnEnable()
         {
-            _cancellationTokenSource = new CancellationTokenSource();
-            Factory = TaskFactoryConfig.CreateFactory(_cancellationTokenSource.Token);
+            if (CreateFactoryLifecycle == CreateLifecycle.OnEnable)
+            {
+                CreateFactoryIfNeeded();
+            }
         }
 
         void OnDisable()
         {
-            _cancellationTokenSource.Cancel();
-            _cancellationTokenSource.Dispose();
+            if (DestroyFactoryLifecycle == DestroyLifecycle.OnDisable)
+            {
+                DestroyFactory();
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (DestroyFactoryLifecycle == DestroyLifecycle.OnDestroy)
+            {
+                DestroyFactory();
+            }
+        }
+
+        public void CreateFactoryIfNeeded()
+        {
+            if (Factory != null)
+            {
+                return;
+            }
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            Factory = TaskFactoryConfig.CreateFactory(_cancellationTokenSource.Token);
+        }
+
+        public void DestroyFactory()
+        {
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+                _cancellationTokenSource.Dispose();
+            }
             Factory = null;
         }
     }
